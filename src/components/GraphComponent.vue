0aa22a30c4e39c46e799fd259247bf9b0425d79c
pelmegov <pelmegovsa@gmail.com>
2019-10-03 23:42:31 +0300

    Added canvas and vertex components, draw some vertexes

--- src/components/GraphComponent.vue
+++ src/components/GraphComponent.vue
@@ -1,16 +1,40 @@
 <template>
   <div class="graph">
     <h1>{{ msg }}</h1>
+    <v-stage ref="stage" :config="stageSize">
+      <VertexComponent />
+      <VertexComponent :x-position="200" :y-position="200" />
+      <VertexComponent :x-position="300" :y-position="300" />
+      <VertexComponent :x-position="400" :y-position="400" />
+      <VertexComponent :x-position="200" :y-position="400" />
+      <VertexComponent :x-position="300" :y-position="500" />
+    </v-stage>
   </div>
 </template>
 
 <script>
+import VertexComponent from './VertexComponent'
+
+const width = window.innerWidth
+const height = window.innerHeight
+
 export default {
   name: 'GraphComponent',
+  components: {
+    VertexComponent
+  },
   props: {
     msg: {
       type: String,
       default: ''
+    }
+  },
+  data: function () {
+    return {
+      stageSize: {
+        width: width,
+        height: height
+      }
     }
   }
 }
---
+++ src/components/VertexComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-layer ref="layer">
+    <v-circle
+      :config="{
+        x: xPosition,
+        y: yPosition,
+        radius: 50,
+        draggable: true,
+        shadowBlur: 5,
+        fill: isDragging ? '#00C851' : '#007E33'
+      }"
+      @dragstart="handleDragStart"
+      @dragend="handleDragEnd"
+    />
+  </v-layer>
+</template>
+
+<script>
+export default {
+  name: 'VertexComponent',
+  props: {
+    xPosition: {
+      type: Number,
+      default: 100
+    },
+    yPosition: {
+      type: Number,
+      default: 100
+    }
+  },
+  data () {
+    return {
+      isDragging: false
+    }
+  },
+  methods: {
+    handleDragStart () {
+      this.isDragging = true
+    },
+    handleDragEnd () {
+      this.isDragging = false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
